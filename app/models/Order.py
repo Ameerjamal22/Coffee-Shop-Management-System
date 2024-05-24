6f7252e648e6ac3c30a7f43a249f1b8fe85dc931
Ameerjamal22 <ameerrabie2008@gmail.com>
2024-05-24 22:36:58 +0300

    adding order model .

---
+++ app/models/Order.py
@@ -0,0 +1,18 @@
+from peewee import *
+from app.models.Customer import Customer
+from app.models.Employee import Employee
+from app.models.Base import BasePeeweeModel
+import datetime
+
+
+class Order(BasePeeweeModel):
+    """
+    Database model that represents the customer order .
+    """
+    order_id = BigAutoField(primary_key=True)
+    customer = ForeignKeyField(Customer, backref="orders", on_delete="CASCADE", null=False)
+    employee = ForeignKeyField(Employee, backref="takes", on_delete="CASCADE", null=False)
+    order_date = DateTimeField(default=datetime.datetime.now())
+    total_price = DecimalField(max_digits=10, decimal_places=2, null=False)
+    status = CharField(max_length=50, choices=["Pending", "Completed", "UnAvailable"], null=False)
+
