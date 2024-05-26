3a778b3cdeae4cfb4b117f524e8405c5c46608a7
Ameerjamal22 <ameerrabie2008@gmail.com>
2024-05-26 13:50:33 +0300

    adding Customer and employee views

---
+++ app/views/InventoryE.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from app.views.ProductE import ProductOut
+
+
+class InventoryBase(BaseModel):
+    """
+    View base model for the inventory.
+    """
+    product: ProductOut = Field(description="the product that the inventory record instance contains .")
+    quantity_in_stock: int = Field(..., ge=0, description="Quantity of the product in stock", examples=[100])
+
+
+class InventoryOut(InventoryBase):
+    """
+    View returned Inventory model.
+    """
+    inventory_id: int
+    last_updated: datetime = Field(default_factory=datetime.now, description="Last updated timestamp of the inventory",
+                                   examples=[datetime.now()])
+
+
+class InventoryIn(InventoryBase):
+    """
+    View input Inventory model.
+    """
+    pass
---
+++ app/views/ProductE.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field, condecimal
+from typing import Optional
+from datetime import datetime
+from app.views.CategoryE import CategoryOut
+
+
+class ProductBase(BaseModel):
+    """
+    View base model for the product.
+    """
+    name: str = Field(..., min_length=1, max_length=80, description="Name of the product", examples=["Latte"])
+    description: Optional[str] = Field(None, description="Description of the product",
+                                       examples=["A rich and creamy coffee drink"])
+    price: condecimal(max_digits=10, decimal_places=2) = Field(..., gt=0, description="Price of the product",
+                                                               examples=[2.99])
+    category: CategoryOut = Field(..., description="Category of the product")
+
+
+class ProductOut(ProductBase):
+    """
+    View returned Product model.
+    """
+    product_id: int
+    creation_date: datetime = Field(default_factory=datetime.now, description="Creation date of the product",
+                                    examples=[datetime.now()])
+
+
+class ProductIn(ProductBase):
+    """
+    View input Product model.
+    """
+    pass
