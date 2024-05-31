da6a7f01ea083c1422e128b0d6d8bbcd724268a8
Ameerjamal22 <ameerrabie2008@gmail.com>
2024-05-31 21:12:03 +0300

    Adding category Apis signatures .

---
+++ app/apis/CategoryApis.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, status
+
+app: FastAPI = FastAPI()
+
+
+@app.post("/categories/", tags=["categories", "AdminUser"], status_code=status.HTTP_204_NO_CONTENT)
+async def create_category():
+    """
+    not implemented yet .
+    """
+    pass
+
+
+@app.get("/categories/", tags=["categories", "AdminUser", "CustomerUser", "EmployeeUser"],
+         status_code=status.HTTP_200_OK)
+async def get_categories():
+    """
+    not implemented yet
+    """
+    pass
+
+
+@app.get("/categories/{category_id}/", tags=["categories", "AdminUser", "CustomerUser", "EmployeeUser"],
+         status_code=status.HTTP_200_OK)
+async def get_category():
+    """
+    not implemented yet
+    """
+    pass
+
+
+@app.put("categories/{category_id}", tags=["categories", "AdminUser"], status_code=status.HTTP_204_NO_CONTENT)
+async def update_category():
+    """
+    not implemented yet
+    """
+    pass
+
+
+@app.delete("categories/{category_id}", tags=["categories", "AdminUser"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_category():
+    """
+    not implemented yet
+    """
+    pass
