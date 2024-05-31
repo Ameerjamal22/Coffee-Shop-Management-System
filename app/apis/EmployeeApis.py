ac9c4f3169cbcf72e940e401d9d02e49a6f23049
Ameerjamal22 <ameerrabie2008@gmail.com>
2024-05-31 20:58:27 +0300

    Adding employee API's signatures .

--- app/apis/EmployeeApis.py
+++ app/apis/EmployeeApis.py
@@ -3,36 +3,36 @@ from fastapi import FastAPI, status
 app: FastAPI = FastAPI()
 
 
-@app.get("/customers/{customer_id}", tags=["customers", "AdminUser", "CustomerUser"],
+@app.get("/employees/{employee_id}", tags=["employees", "AdminUser", "EmployeeUser"],
          status_code=status.HTTP_200_OK)
-async def get_customer():
+async def get_employee():
     """
     not implemented yet .
     """
     pass
 
 
-@app.get("/customers", tags=["customers", "AdminUser"],
+@app.get("/employees", tags=["employees", "AdminUser"],
          status_code=status.HTTP_200_OK)
-async def get_customer():
+async def get_employee():
     """
     not implemented yet .
     """
     pass
 
 
-@app.put("/customers/{customer_id}", tags=["customers", "AdminUser", "CustomerUser"],
+@app.put("/customers/{customer_id}", tags=["employees", "AdminUser"],
          status_code=status.HTTP_204_NO_CONTENT)
-async def update_customer():
+async def update_employee():
     """
     not implemented yet .
     """
     pass
 
 
-@app.delete("/customers/{customer_id}", tags=["customers", "AdminUser", "CustomerUser"],
+@app.delete("/customers/{customer_id}", tags=["employees", "AdminUser"],
             status_code=status.HTTP_204_NO_CONTENT)
-async def delete_customer():
+async def delete_employee():
     """
     not implemented yet .
     """
