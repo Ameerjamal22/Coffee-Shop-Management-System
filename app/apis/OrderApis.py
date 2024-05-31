d4031f9e1c948c88f98e9b6e6c5771c7150dac78
Ameerjamal22 <ameerrabie2008@gmail.com>
2024-05-31 22:04:17 +0300

    Adding order Apis signatures .

---
+++ app/apis/OrderApis.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, status
+
+app: FastAPI = FastAPI()
+
+
+@app.post("/orders/", tags=["orders", "AdminUser", "EmployeeUser", "CustomerUser"],
+          status_code=status.HTTP_201_CREATED)
+async def create_order():
+    """
+    not yet implemented
+    """
+    pass
+
+
+@app.get("/orders/", tags=["orders", "AdminUser", "EmployeeUser"], status_code=status.HTTP_200_OK)
+async def get_orders():
+    """
+    not yet implemented
+    """
+    pass
+
+
+@app.get("/orders/{order_id}/", tags=["orders", "AdminUser", "EmployeeUser", "CustomerUser"],
+         status_code=status.HTTP_200_OK)
+async def get_order():
+    """
+    not implemented yet
+    """
+    pass
+
+
+@app.put("/orders/{order_id}/", tags=["orders", "AdminUser", "EmployeeUser"],
+         status_code=status.HTTP_204_NO_CONTENT)
+async def update_order():
+    """
+    not yet implemented
+    """
+    pass
+
+
+@app.delete("/orders/{order_id}/", tags=["orders", "AdminUser"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_order():
+    """
+    not yet implemented
+    """
+    pass
